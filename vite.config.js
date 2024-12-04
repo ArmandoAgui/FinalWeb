b8fe7cf2af7a1ed9dcb421187bde79d019100a04
ArmandoAgui <00174323@uca.edu.sv>
2024-12-04 05:30:12 -0600

    probando

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'build', // Changes the output directory to "build"
-  },
+    outDir: 'dist'
+  }
 });
