cdf8b0910c3a0b1a0145cbdfdc83879236f1bcef
ArmandoAgui <00174323@uca.edu.sv>
2024-12-04 09:36:52 -0600

    probando2

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), pugPlugin()],
+  plugins: [react()],  // Solo el plugin de React ahora
   build: {
     outDir: 'dist', // Asegura que los archivos se generen en la carpeta 'dist' para el deploy
     assetsDir: 'assets', // Directorio para los archivos estáticos generados
     sourcemap: false, // Opcional: puedes desactivar los mapas de fuente en producción si no los necesitas
-    // Puedes agregar más configuraciones aquí si lo necesitas
   },
   server: {
     port: 3000, // Puerto en el que se ejecutará el servidor de desarrollo
