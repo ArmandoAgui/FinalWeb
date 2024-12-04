27e34d82e4076610445b989694efc46df578e4f3
ArmandoAgui <00174323@uca.edu.sv>
2024-12-04 04:58:07 -0600

    probando

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import pugPlugin from 'vite-plugin-pug';
 
 export default defineConfig({
-  plugins: [react(), pugPlugin()],
+  build: {
+    outDir: 'build', // Changes the output directory to "build"
+  },
 });
