d359f5950ec2cb2b179ac3549c8e850d6ff3d573
ArmandoAgui <00174323@uca.edu.sv>
2024-12-04 00:31:49 -0600

    vamo

--- src/pages/client/Perfil.jsx
+++ src/pages/client/Perfil.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faLock } from '@fortawesome/free-solid-svg-icons';
 import '../../assets/styles/Pedidos.scss';
-import Pedidos from './pedidos';
+import Pedidos from '../../components/admin/pedidos';
 import { useNavigate } from 'react-router-dom';
 import { getProfile, updateProfile } from '../../services/client/profileService';
 import userImage from '../../assets/images/user.png';
