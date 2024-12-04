7bb8c90bcd86bf4f57b8dabcacde159a60454022
ArmandoAgui <00174323@uca.edu.sv>
2024-12-04 09:34:26 -0600

    probanding

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pugPlugin()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist', // Asegura que los archivos se generen en la carpeta 'dist' para el deploy
+    assetsDir: 'assets', // Directorio para los archivos estáticos generados
+    sourcemap: false, // Opcional: puedes desactivar los mapas de fuente en producción si no los necesitas
+    // Puedes agregar más configuraciones aquí si lo necesitas
+  },
+  server: {
+    port: 3000, // Puerto en el que se ejecutará el servidor de desarrollo
+  },
+  base: './', // Asegura que las rutas relativas funcionen correctamente, especialmente si usas React Router
 });
